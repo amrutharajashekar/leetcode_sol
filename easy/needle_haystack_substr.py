328a12d9367d85b8ae0de2cc382171922544d74c
amrutha <amruthar1098@gmail.com>
2022-08-15 00:04:51 +0530

    no need ot revise

---
+++ easy/needle_haystack_substr.py
@@ -0,0 +1,17 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+                
+        i, j = 0, 0
+                    
+        while i < len(haystack) and j < len(needle) :
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+                
+                if j == len(needle) :
+                    return i - j 
+            else:
+                i = i - j + 1
+                j = 0 
+            
+        return -1
